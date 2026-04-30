3fdcc783e0d5cb2b82fc81922a68d0a8ee873934
Jamie Mansfield <jmansfield@cadixdev.org>
2026-04-30 11:03:17 +0100

    Add views to create/view users in admin pages

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   # Admin routes
   namespace :admin do
     resources :items
+    resources :users
     resources :sales, only: [ :index, :show ]
 
     resources :tills do
