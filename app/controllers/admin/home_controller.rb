b48456f6d067aac0a35db3ce7806761641c7045d
Jamie Mansfield <jmansfield@cadixdev.org>
2026-04-30 11:16:03 +0100

    Show payment breakdown on admin dashboard

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -5,6 +5,10 @@ module Admin
       @user = Current.user
       @active_sessions = TillSession.active
       @todays_sales = Sale.where(completed_at: Date.current.all_day)
+      @todays_payments = Payment.joins(:sale)
+                                .where(sales: { completed_at: Date.current.all_day })
+                                .group(:method)
+                                .sum(:amount)
     end
   end
 end
